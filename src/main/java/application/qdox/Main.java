eceffb5dd51dd237dc90ffaa0655f3a533b73e77
skuarch <skuarch@yahoo.com.mx>
2017-07-21 21:53:40 -0500

    init

---
+++ src/main/java/application/qdox/Main.java
@@ -0,0 +1,12 @@
+package application.qdox;
+
+import com.thoughtworks.qdox.JavaDocBuilder;
+import java.io.File;
+
+
+public class Main {
+
+    File sourceFolder = new File(".");
+    JavaDocBuilder parser = new JavaDocBuilder();
+
+}
